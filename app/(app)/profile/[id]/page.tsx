d1c64ad08ba39a8e964cb7c2c98a4a73f7a57dd6
ShellyDCMS <shelly.goldblit@dell.com>
2026-05-10 12:09:41 +0300

    fix build

--- app/(app)/profile/[id]/page.tsx
+++ app/(app)/profile/[id]/page.tsx
@@ -1,6 +1,6 @@
 import FollowButton from "@/components/follow-button/follow-button";
 import { auth, signOut } from "@/lib/auth";
-import { databaseService } from "@/lib/services";
+import { databaseService, videoService } from "@/lib/services";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
